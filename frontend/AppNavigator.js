62ca3bb889e324b5bf68a48e893301a261d7194c
Nicky Lim <nickylim.p@gmail.com>
2021-10-07 14:56:25 +0800

    integrate existing screens

---
+++ frontend/AppNavigator.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Icon,
+} from "@ui-kitten/components";
+
+import Register from "./screens/Register";
+import Login from "./screens/Login";
+import Bmi from "./screens/Bmi";
+import Ippt from "./screens/Ippt";
+import Startup from "./screens/Startup";
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const HomeNavigator = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Startup" component={Startup} />
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Register" component={Register} />
+  </Stack.Navigator>
+);
+
+// TODO Create tab icons (NOTE: no icons for Bmi and Ippt in Eva Icons, might need to import 3rd party icons)
+
+const HomeIcon = (props) => <Icon {...props} name="home" />;
+
+const BottomTabBar = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={(index) => navigation.navigate(state.routeNames[index])}
+  >
+    <BottomNavigationTab title="BMI" />
+    <BottomNavigationTab title="Home" icon={HomeIcon} />
+    <BottomNavigationTab title="IPPT" />
+  </BottomNavigation>
+);
+
+const BottomTabNavigator = () => (
+  <Tab.Navigator
+    tabBar={(props) => <BottomTabBar {...props} />}
+    screenOptions={{ headerShown: false }}
+    initialRouteName="Home"
+  >
+    <Tab.Screen name="Bmi" component={Bmi} />
+    <Tab.Screen name="Home" component={HomeNavigator} />
+    <Tab.Screen name="Ippt" component={Ippt} />
+  </Tab.Navigator>
+);
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <BottomTabNavigator />
+  </NavigationContainer>
+);
