d45560234c58e34191b242063d473e6604bf92a7
Qi Xiang <qixyqix@outlook.com>
2021-10-15 03:02:41 +0800

    Add login service, router and controller

--- server/src/controller/authController.ts
+++ server/src/controller/authController.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from 'express';
 import AuthService from '../service/authService';
 
-const Register = async (req:Request, res:Response) => {
-  const {
-    email, name, password
-  } = req.body;
+const Register = async (req: Request, res: Response) => {
+  const { email, name, password } = req.body;
 
   try {
     const tokens = await AuthService.Register(email, name, password);
@@ -14,9 +12,21 @@ const Register = async (req:Request, res:Response) => {
   }
 };
 
+const Login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await AuthService.Login(email, password);
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
 
 const AuthController = {
   Register,
+  Login
 };
 
 export { AuthController as default };
