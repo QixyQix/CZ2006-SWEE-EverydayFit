ec97dd3c786b19a9084559343197c9391edc0453
Mikealeee <michael.yewchuan@gmail.com>
2021-10-14 12:49:49 +0800

    Added functionality to confirm logout

--- frontend/components/HomeHeader.js
+++ frontend/components/HomeHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
 import tailwind from "tailwind-rn";
 import DateWeatherButton from "./DateWeatherButton";
-import { Layout, Text } from "@ui-kitten/components";
+import { Layout, Text, Button, Card, Modal } from "@ui-kitten/components";
 import { TouchableOpacity } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
@@ -44,20 +44,35 @@ const CalendarButton = () => {
 const LogoutButton = () => {
   const navigation = useNavigation();
 
+  const [visible, setVisible] = React.useState(false);
+
+  const LogoutIcon = (props) => (
+    <FontAwesome5 style={tailwind("pb-1")} name = 'power-off' size = {15} color = 'black' />
+  );
+
   return (
     
-    <TouchableOpacity 
-      style={tailwind("items-center")}
-      onPress= {() =>  navigation.navigate('Startup')}
-    >  
-    <FontAwesome5 
-      style={tailwind("pb-1")}
-      name = 'power-off' 
-      size = {25} 
-      color = 'black' 
-    />   
-     <Text style={tailwind("font-bold")} > Logout </Text>
-    </TouchableOpacity>
+    <Layout>
+      <Button style={tailwind("bg-red-400")}  accessoryLeft = { LogoutIcon  } onPress={() => setVisible(true)}>
+        Logout
+      </Button>
+    
+      <Modal visible={visible} >
+        <Card disabled={true}  >
+          <Text> Are you sure you want to logout </Text>
+          <Layout style={tailwind("flex-row justify-center items-center ")}>
+          <Button onPress={() => setVisible(false)}>
+            No
+          </Button>
+
+          <Button onPress={() => setVisible(false)} onPress={ () => navigation.navigate('Startup')}>
+            Confirm
+          </Button>
+          </Layout> 
+        </Card>
+      </Modal>
+
+  </Layout>
 
   );
 };
