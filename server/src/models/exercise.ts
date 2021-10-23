ff77aef8370450d2690f9ac468ba5bd849576691
Qi Xiang <qixyqix@outlook.com>
2021-10-23 21:37:31 +0800

    Update exercise model and exerciseRepo

--- server/src/models/exercise.ts
+++ server/src/models/exercise.ts
@@ -1,18 +1,19 @@
 import mongoose from 'mongoose';
-import { EQuantityType } from './constants';
+import { EExerciseCategory, EQuantityType } from './constants';
 
 export interface IExercise extends mongoose.Document {
     _id: string,
     name: string,
     category: string,
     outdoorOnly: boolean,
+    quantityType: string,
     quantityUnit: string,
     calorieBurnRatePerUnit: number
 }
 
 const exerciseSchema = new mongoose.Schema({
     name: String,
-    category: String,
+    category: {type: String, enum: Object.values(EExerciseCategory)},
     outdoorOnly: Boolean,
     quantityType: { type: String, enum: Object.values(EQuantityType) },
     quantityUnit: String,
