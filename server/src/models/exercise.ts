6adeb9ec755c3aaa71c1c500525b46690b635fb4
Qi Xiang <qixyqix@outlook.com>
2021-10-23 21:24:44 +0800

    Move Exercise Seed and seeder into data folder, fix seed and introduced enums

--- server/src/models/exercise.ts
+++ server/src/models/exercise.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
+import { EQuantityType } from './constants';
 
 export interface IExercise extends mongoose.Document {
     _id: string,
     name: string,
     category: string,
-    isOutdoor: string,
+    outdoorOnly: boolean,
     quantityUnit: string,
     calorieBurnRatePerUnit: number
 }
@@ -12,7 +13,8 @@ export interface IExercise extends mongoose.Document {
 const exerciseSchema = new mongoose.Schema({
     name: String,
     category: String,
-    isOutdoor: String,
+    outdoorOnly: Boolean,
+    quantityType: { type: String, enum: Object.values(EQuantityType) },
     quantityUnit: String,
     calorieBurnRatePerUnit: Number,
 });
