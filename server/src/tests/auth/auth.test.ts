02e499770b53f60eb349cd15d247f74ddcda587d
Qi Xiang <qixyqix@outlook.com>
2021-11-07 18:13:52 +0800

    Add test invalid email

--- server/src/tests/auth/auth.test.ts
+++ server/src/tests/auth/auth.test.ts
@@ -1,8 +1,9 @@
 import testDB from "../mongoTestDB";
 import AuthService from "../../service/authService";
 import AuthController from "../../controller/authController";
-import { getMockReq, getMockRes } from "@jest-mock/express";
 import { Request, Response } from "express";
+import app from '../../app';
+import request from "supertest";
 
 beforeAll(async () => {
     await testDB.connect();
@@ -16,24 +17,11 @@ afterAll(async () => {
 });
 
 describe('AUTH: Register an account', () => {
-    it('throws an error when invalid email format is used', async () => {
-        const req = getMockReq({
-            body: {
-                email: "qixyqix@Outlook.com",
-                name: "Qi Xiang",
-                password: "P@ssw0rd"
-            }
-        }) as Request;
-        const { res } = getMockRes();
-        await AuthController.Register(req, res);
-        console.log(res);
-        expect(res.status).toHaveBeenCalledWith(500);
-        expect(res.json).toHaveBeenCalledWith({ message: 'Email must be in name@email.com format' });
-    })
-    // it('Registers account', async () => {
-    //     const result = await AuthService.Register("qixyqix@outlook.com", "Qi Xiang", "P@ssw0rd");
-
-    //     expect(result.token).not.toEqual("");
-    //     expect(result.refreshToken).not.toEqual("");
-    // });
+    it('throws error when invalid emial format is used', async () => {
+        await request(app).post('/auth/register').send({
+            email: "qixyqixOutlook.com",
+            name: "Qi Xiang",
+            password: "P@ssw0rd"
+        }).expect(500)
+    });
 })
