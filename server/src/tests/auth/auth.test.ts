86f0264fdc05cedf9fae4902fef2bbf914b97e6c
Qi Xiang <qixyqix@outlook.com>
2021-11-07 16:48:36 +0800

    Try using mock response
    Not working

--- server/src/tests/auth/auth.test.ts
+++ server/src/tests/auth/auth.test.ts
@@ -1,5 +1,8 @@
 import testDB from "../mongoTestDB";
 import AuthService from "../../service/authService";
+import AuthController from "../../controller/authController";
+import { getMockReq, getMockRes } from "@jest-mock/express";
+import { Request, Response } from "express";
 
 beforeAll(async () => {
     await testDB.connect();
@@ -13,10 +16,24 @@ afterAll(async () => {
 });
 
 describe('AUTH: Register an account', () => {
-    it('Registers account', async () =>{
-        const result = await AuthService.Register("qixyqix@outlook.com","Qi Xiang","P@ssw0rd");
+    it('throws an error when invalid email format is used', async () => {
+        const req = getMockReq({
+            body: {
+                email: "qixyqix@Outlook.com",
+                name: "Qi Xiang",
+                password: "P@ssw0rd"
+            }
+        }) as Request;
+        const { res } = getMockRes();
+        await AuthController.Register(req, res);
+        console.log(res);
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ message: 'Email must be in name@email.com format' });
+    })
+    // it('Registers account', async () => {
+    //     const result = await AuthService.Register("qixyqix@outlook.com", "Qi Xiang", "P@ssw0rd");
 
-        expect(result.token).not.toEqual("");
-        expect(result.refreshToken).not.toEqual("");
-    });
+    //     expect(result.token).not.toEqual("");
+    //     expect(result.refreshToken).not.toEqual("");
+    // });
 })
